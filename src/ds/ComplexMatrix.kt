120b5a708a0496f1e50a7075609df4c7b9912102
AaronSong321 <2511146542@qq.com>
2020-06-25 14:25:22 +0800

    Finish fft algorithms and related functions.

--- src/ds/ComplexMatrix.kt
+++ src/ds/ComplexMatrix.kt
@@ -5,25 +5,35 @@ import kotlin.math.*
 
 class CoefficientPolynomial(val degree: Int, init: (Int) -> Complex = { ZERO }) {
     private val coefficient = Array<Complex>(degree) { init(it) }
-    val regularOrder: Int
+
+    /**
+     * Gets 2^{log n}, where n stands for the degree of this polynomial
+     */
+    val regularDegree: Int
         get() {
             val d = ceil(log(degree.toDouble(), 2.toDouble()))
             return 2.toDouble().pow(d).toInt()
         }
+
     operator fun get(a: Int) = coefficient[a]
     operator fun set(a: Int, v: Complex) {
         coefficient[a] = v
     }
 
+    /**
+     * Extend the coefficient polynomial into one with more dummy terms. Usually used before fourier transformation.
+     * @param newDegree The target degree
+     * @return The expanded polynomial
+     */
     fun extendTo(newDegree: Int): CoefficientPolynomial {
         return CoefficientPolynomial(newDegree) { if (it < degree) get(it) else ZERO }
     }
     fun extendToRegular(): CoefficientPolynomial {
-        val f = regularOrder
+        val f = regularDegree
         return if (f > degree) extendTo(f) else this
     }
 
-    fun recursiveFft(targetDegree: Int = regularOrder): ValuePolynomial {
+    fun recursiveFft(targetDegree: Int = regularDegree): ValuePolynomial {
         val s = extendTo(targetDegree)
         if (targetDegree == 1)
             return ValuePolynomial(1) { s[0] }
@@ -58,14 +68,24 @@ class CoefficientPolynomial(val degree: Int, init: (Int) -> Complex = { ZERO }) 
         val extendOther = other.extendTo(r)
         return CoefficientPolynomial(r) { extendThis[it] + extendOther[it] }
     }
+    operator fun minus(other: CoefficientPolynomial): CoefficientPolynomial {
+        val r = max(degree, other.degree)
+        val extendThis = extendTo(r)
+        val extendOther = other.extendTo(r)
+        return CoefficientPolynomial(r) { extendThis[it] - extendOther[it] }
+    }
     operator fun times(other: CoefficientPolynomial): CoefficientPolynomial {
-        val r = max(regularOrder, other.regularOrder)
+        val r = max(regularDegree, other.regularDegree)
         val fftThis = extendTo(r + r).iterativeFft()
         val fftOther = other.extendTo(r + r).iterativeFft()
-        return (fftThis * fftOther).inverseRecursiveFft().truncate()
+        return (fftThis * fftOther).inverseIterativeFft().shrink()
     }
 
-    fun truncate(): CoefficientPolynomial {
+    /**
+     * Shrink the polynomial to get rid of terms with the highest degree whose coefficients are 0
+     * @return the shrunk polynomial
+     */
+    fun shrink(): CoefficientPolynomial {
         var lastNonZeroIndex = degree
         while (lastNonZeroIndex >= 0) {
             val co = coefficient[lastNonZeroIndex-1]
@@ -76,7 +96,7 @@ class CoefficientPolynomial(val degree: Int, init: (Int) -> Complex = { ZERO }) 
         return CoefficientPolynomial(lastNonZeroIndex) { coefficient[it] }
     }
 
-    fun iterativeFft(targetDegree: Int = regularOrder): ValuePolynomial {
+    fun iterativeFft(targetDegree: Int = regularDegree): ValuePolynomial {
         val ext = extendTo(targetDegree)
         val digit = ceil(log(targetDegree.toDouble(), 2.toDouble())).toInt()
         val copy = ValuePolynomial(targetDegree) { ext[bitReverse(it, digit)] }
@@ -99,6 +119,56 @@ class CoefficientPolynomial(val degree: Int, init: (Int) -> Complex = { ZERO }) 
         }
         return copy
     }
+
+    infix fun shr(a: Int): CoefficientPolynomial {
+        if (a < 0)
+            throw IllegalArgumentException("shr $a < 0")
+        return CoefficientPolynomial(degree + a) { if (it < a) 0.R else get(it - a) }
+    }
+
+    fun slices(a: IntRange): CoefficientPolynomial {
+        if (a.first < 0)
+            throw IndexOutOfBoundsException("slices a.first = ${a.first} < 0")
+        if (a.last >= degree)
+            throw IndexOutOfBoundsException("slices a.first = ${a.last} > $degree")
+        val size = a.last - a.first + 1
+        return CoefficientPolynomial(size) { this[it + a.first] }
+    }
+
+    operator fun get(a: IntRange): CoefficientPolynomial = slices(a)
+
+    private fun innerNormalTimes(other: CoefficientPolynomial): CoefficientPolynomial {
+        return when(degree) {
+            1 -> {
+                coefficientPolyOf(this[0] * other[0])
+            }
+            2 -> {
+                val p1 = this[0] * other[0]
+                val p2 = this[1] * other[1]
+                val p3 = (this[0] + this[1]) * (other[0] + other[1])
+                coefficientPolyOf(p1, p3-p1-p2, p2)
+            }
+            else -> {
+                val hd = degree shr 1
+                val term1 = this[0 until hd]
+                val term2 = this[hd until degree]
+                val term3 = other[0 until hd]
+                val term4 = other[hd until degree]
+                val p1 = term2.innerNormalTimes(term4)
+                val p2 = term1.innerNormalTimes(term3)
+                val p3 = (term1+term2).innerNormalTimes(term3+term4)
+                val c2 = p3 - p1 - p2
+                p2 + (c2 shr hd) + (p1 shr degree)
+            }
+        }
+    }
+
+    fun normalTimes(other: CoefficientPolynomial): CoefficientPolynomial {
+        val a = max(regularDegree, other.regularDegree)
+        val b = extendTo(a)
+        val c = other.extendTo(a)
+        return b.innerNormalTimes(c).shrink()
+    }
 }
 
 fun bitReverse(a: Int, digit: Int): Int {
@@ -112,6 +182,8 @@ fun bitReverse(a: Int, digit: Int): Int {
 }
 
 fun coefficientPolyOf(vararg a: Complex): CoefficientPolynomial {
+    if (a.isEmpty())
+        throw IllegalArgumentException("arg number == 0")
     return CoefficientPolynomial(a.size) { a[it] }
 }
 
@@ -172,27 +244,26 @@ class ValuePolynomial(val degree: Int, init: (Int) -> Complex = { ZERO }) {
     }
 
     fun inverseIterativeFft(): CoefficientPolynomial {
-        throw NotImplementedError("Not finished yet")
-//        val digit = ceil(log(degree.toDouble(), 2.toDouble())).toInt()
-//        val copy = CoefficientPolynomial(degree) { values[it] }
-//
-//        var m = 1
-//        for (s in 0 until digit) {
-//            m = m shl 1
-//            val deltaOmega = pow(E, (-2).I * PI / m)
-//            for (k in 0 until degree step m) {
-//                var omega = 1.R
-//                val halfM = m shr 1
-//                for (j in 0 until halfM) {
-//                    val t = omega * copy[k + j + halfM]
-//                    val u = copy[k + j]
-//                    copy[k + j] = u + t
-//                    copy[k + j + halfM] = u - t
-//                    omega *= deltaOmega
-//                }
-//            }
-//        }
-//        return CoefficientPolynomial(degree) { copy[bitReverse(it, digit)] / degree }
+        val digit = ceil(log(degree.toDouble(), 2.toDouble())).toInt()
+        val copy = CoefficientPolynomial(degree) { values[bitReverse(it, digit)] }
+
+        var m = 1
+        for (s in 0 until digit) {
+            m = m shl 1
+            val deltaOmega = pow(E, (-2).I * PI / m)
+            for (k in 0 until degree step m) {
+                var omega = 1.R
+                val halfM = m shr 1
+                for (j in 0 until halfM) {
+                    val t = omega * copy[k + j + halfM]
+                    val u = copy[k + j]
+                    copy[k + j] = u + t
+                    copy[k + j + halfM] = u - t
+                    omega *= deltaOmega
+                }
+            }
+        }
+        return CoefficientPolynomial(degree) { copy[it] / degree }
     }
 }
 
@@ -213,7 +284,7 @@ fun calculatePolynomialFromZeroPoint(zeros: List<Complex>): CoefficientPolynomia
         if (it < oldOrder) coefficientPolyOf(-zeros[it], 1.R)
         else coefficientPolyOf(1.R)
     }
-    return t.product().truncate()
+    return t.product().shrink()
 }
 
 fun testFFT() {
@@ -221,7 +292,9 @@ fun testFFT() {
     val c2 = coefficientPolyOf(3.R, (-6).R, 8.R)
     println(c1.recursiveFft().inverseRecursiveFft())
     println(c1.iterativeFft().inverseRecursiveFft())
+    println(c1.iterativeFft().inverseIterativeFft())
     println(c1 * c2)
-    println(c2.recursiveFft(8).inverseRecursiveFft().truncate())
+    println(c1.normalTimes(c2))
+    println(c2.recursiveFft(8).inverseRecursiveFft().shrink())
     println(calculatePolynomialFromZeroPoint(listOf(3.R,6.R,3.R,8.R,7.R)))
 }
